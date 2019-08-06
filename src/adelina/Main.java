254113c029537dfe5a0eb3da863a358a91d29486
AdelinaNuritdinova <52155675+AdelinaNuritdinova@users.noreply.github.com>
2019-08-06 21:20:38 +0600

    Update Main.java

--- src/adelina/Main.java
+++ src/adelina/Main.java
@@ -5,10 +5,6 @@ import java.util.concurrent.CountDownLatch;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
-        new Guide(countDownLatch);
-
-        countDownLatch.await();
-        System.out.println("done");
-
+        countDownLatch.countDown();
+        }
     }
-}
