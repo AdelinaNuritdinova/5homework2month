8793f8b03e5e18c320a295f832b2aed81b894605
Adelina <adelya.n.01@gmail.com>
2019-08-07 15:38:37 +0600

    Внесены изменения в класс Guide ; Main

--- src/adelina/Guide.java
+++ src/adelina/Guide.java
@@ -4,20 +4,31 @@ import java.util.concurrent.CountDownLatch;
 
 public class Guide extends Thread {
     CountDownLatch countDownLatch;
+    int humans;
+
+    public Guide(CountDownLatch countDownLatch, int humans) {
+        this.countDownLatch = countDownLatch;
+        this.humans = humans;
+    }
 
     @Override
             public void run(){
         try {
-            countDownLatch.await();
-            System.out.println("done");
-            for(int i = 1 ; i <= 15 ; i++){
-                System.out.println(i);
+            System.out.println(humans);
+            countDownLatch.countDown();
+            if(humans < 15){
+
+
+//            for(int i = 1 ; i <= 15 ; i++){
+//                System.out.println(i);
+
             }
-            sleep(3000);
+            countDownLatch.await();
+
         }catch (InterruptedException e){
             e.printStackTrace();
         }
-        System.out.println("Done Work");
+//        System.out.println("Done Work");
 
     }
 }
--- src/adelina/Main.java
+++ src/adelina/Main.java
@@ -4,7 +4,14 @@ import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        countDownLatch.countDown();
+        CountDownLatch countDownLatch = new CountDownLatch(15);
+        for (int i = 1; i <= 15; i++) {
+            Guide guide = new Guide(countDownLatch, i);
+            guide.start();
         }
+        countDownLatch.await();
+        System.out.println("Группа собрана");
+        System.out.println("Экскурсия началась");
+
     }
+}
